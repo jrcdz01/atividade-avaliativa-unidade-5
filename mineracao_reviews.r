86152ca9884a8ae9ee63bcfb1d1de13f6aad43c1
jrcdz01 <jrcdz01@gmail.com>
2021-06-24 21:49:48 -0300

    Update mineracao_reviews.r

--- mineracao_reviews.r
+++ mineracao_reviews.r
@@ -57,17 +57,3 @@ bad <- reviews.afinn %>%
   filter(mean_rating<mean(mean_rating)) %>%
   arrange(mean_rating)
 wordcloud(words = bad$word, freq = bad$count_word, scale=c(5,.5), max.words=100, colors=brewer.pal(8, "Set2"))
-
-afinn <- get_sentiments("bing") %>% mutate(word = wordStem(word))
-reviews.bing <- words %>%
-  inner_join(bing, by = "word")
-head(reviews.afinn)
-
-
-
-library(syuzhet)
-sentimento <- get_nrc_sentiment(reviews) %>%
-  select(c("description")) %>%
-   
-
-barplot(colSums(sentimento),las = 2,ylab = "Quantidade",main = "Sentimento")
